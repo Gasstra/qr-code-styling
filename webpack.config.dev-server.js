100c984d1c2cce6a22b95a7b6647da96e4771194
sshapoval <shapovalstas@gmail.com>
2023-01-17 23:52:49 +0200

    test

--- webpack.config.dev-server.js
+++ webpack.config.dev-server.js
@@ -4,9 +4,6 @@ const commonConfig = require('./webpack.config.common.js');
 
 module.exports = merge(commonConfig, {
   mode: 'development',
-  devServer: {
-    injectClient: false //workaround for bug https://github.com/webpack/webpack-dev-server/issues/2484
-  },
   devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
